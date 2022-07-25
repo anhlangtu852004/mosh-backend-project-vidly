54ecca1192481301b6f8083400eb11a8b278294d
tri <tridesignnm@gmal.com>
2022-07-25 18:18:55 +0700

    toi log roi

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
+const express = require("express");
 const mongoose = require("mongoose");
 require("express-async-errors");
 require("dotenv").config();
+const winston = require("winston");
+
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const user = require("./routes/users");
 const authen = require("./routes/authens");
-const express = require("express");
-const app = express();
-
 const error = require("./middleware/error");
+
+const app = express();
 
 // if (!config.get("jwtPrivateKey")) {
 //   console.error("jwtPrivateKey is not define");
 //   process.exit(1);
 // }
+
+// process.on("uncaughtException", (ex) => {
+//   console.log("we got uncaughtException.....");
+//   winston.error(ex.message, ex);
+// });
+
+winston.handleException(
+  new winston.transports.File({ filename: "uncaughtException.log" })
+);
+
+//khi ma promise bij reject thif process.on chup va quang throw ex. khi quang throw ex thi thang
+// winston handleException chup lay
+process.on("unhandledRejection", (ex) => {
+  // console.log("we got unhandle rejection.....");
+  // winston.error(ex.message, ex);
+  throw ex;
+});
+
+winston.add(new winston.transports.File({ filename: "logfile.log" }));
 
 if (!process.env.vidly_jwtPrivateKey) {
   console.error("jwtPrivateKey is not define");
--- middleware/error.js
+++ middleware/error.js
@@ -1,3 +1,6 @@
-module.exports = function (error, req, res, next) {
+const winston = require("winston");
+
+module.exports = function (err, req, res, next) {
+  winston.error(err.message, err);
   res.status(500).send("something failse");
 };
